793e0ca439b51603449af7db99700e0545b3939c
DanishJamal <danishjamal.104@gmail.com>
2022-07-07 15:24:25 +0530

    Basic project setup

---
+++ src/main/java/org/mifos/integrationtest/IntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package org.mifos.integrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mifos/integrationtest/PhEeConnectorIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mifos.integrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhEeConnectorIntegrationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
