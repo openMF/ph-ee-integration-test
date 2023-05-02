55d5163891ad97dc71b146ccb1b4e9cbabd2337b
Arka Das <arka.das@fynarfin.io>
2023-05-03 00:50:46 +0530

    remove unused imports

--- src/test/java/org/mifos/integrationtest/cucumber/stepdef/ZeebeStepDef.java
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/ZeebeStepDef.java
@@ -1,6 +1,5 @@
 package org.mifos.integrationtest.cucumber.stepdef;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import io.cucumber.java.en.And;
