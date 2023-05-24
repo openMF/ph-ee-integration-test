aa3bcc73aa15ba658785869ca8e21cf4950f77e1
Ankita Ranjan <92036361+ankita10r@users.noreply.github.com>
2023-05-24 18:44:12 +0530

    GOV-241 (#40)

--- src/test/java/org/mifos/integrationtest/cucumber/stepdef/BatchApiStepDef.java
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/BatchApiStepDef.java
@@ -7,7 +7,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.mapper.ObjectMapperType;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
 import org.mifos.integrationtest.common.Utils;
 import org.mifos.integrationtest.common.dto.BatchApiResponseDTO;
 
@@ -45,7 +49,7 @@ public class BatchApiStepDef extends BaseStepDef {
         if (authEnabled) {
             requestSpec.header("Authorization", "Bearer " + BaseStepDef.accessToken);
         }
-        requestSpec.queryParam("batchId", BaseStepDef.batchId);
+        // requestSpec.queryParam("batchId", BaseStepDef.batchId);
         logger.info("Calling with batch id: {}", BaseStepDef.batchId);
 
         BaseStepDef.response = RestAssured.given(requestSpec)
@@ -53,7 +57,7 @@ public class BatchApiStepDef extends BaseStepDef {
                 .expect()
                 .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build())
                 .when()
-                .get(operationsAppConfig.batchSummaryEndpoint)
+                .get(operationsAppConfig.batchSummaryEndpoint + "?batchId=" + BaseStepDef.batchId)
                 .andReturn().asString();
 
         logger.info("Batch Summary Response: " + BaseStepDef.response);
@@ -87,14 +91,14 @@ public class BatchApiStepDef extends BaseStepDef {
         BaseStepDef.response = RestAssured.given(requestSpec)
                 .baseUri(bulkProcessorConfig.bulkProcessorContactPoint)
                 .contentType("multipart/form-data")
-                .multiPart("data", Utils.getAbsoluteFilePathToResource(BaseStepDef.filename))
+                .multiPart("file", new File(Utils.getAbsoluteFilePathToResource(BaseStepDef.filename)))
                 .expect()
                 .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build())
                 .when()
                 .post(bulkProcessorConfig.bulkTransactionEndpoint)
                 .andReturn().asString();
 
-        logger.info("Batch Details Response: " + BaseStepDef.response);
+        logger.info("Batch Transactions API Response: " + BaseStepDef.response);
     }
 
     @Then("I should get non empty response")
@@ -112,6 +116,33 @@ public class BatchApiStepDef extends BaseStepDef {
     public void iShouldHaveAndInResponse(String pollingpath, String suggestedcallback) {
         assertThat(BaseStepDef.response).contains(pollingpath);
         assertThat(BaseStepDef.response).contains(suggestedcallback);
+
+    }
+
+    @When("I call the batch transactions endpoint with expected status of {int} without payload")
+    public void iCallTheBatchTransactionsEndpointWithExpectedStatusOfWithoutPayload(int expectedStatus) {
+        RequestSpecification requestSpec = Utils.getDefaultSpec(BaseStepDef.tenant);
+        requestSpec.header("filename", BaseStepDef.filename);
+        requestSpec.header("X-CorrelationID", UUID.randomUUID().toString());
+        requestSpec.queryParam("type", "CSV");
+        BaseStepDef.response = RestAssured.given(requestSpec)
+                .baseUri(bulkProcessorConfig.bulkProcessorContactPoint)
+                .expect()
+                .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build())
+                .when()
+                .post(bulkProcessorConfig.bulkTransactionEndpoint)
+                .andReturn().asString();
+
+        logger.info("Batch Transactions without payload Response: " + BaseStepDef.response);
+    }
+
+    @And("I should get batchId in response")
+    public void iShouldGetBatchIdInResponse() throws JSONException {
+        JSONObject jsonObject = new JSONObject(BaseStepDef.response);
+        String pollingPath = (String) jsonObject.get("PollingPath");
+        String[] response = pollingPath.split("/");
+        logger.info("Batch Id: {}", response[response.length - 1]);
+        BaseStepDef.batchId = response[response.length - 1];
 
     }
 }
