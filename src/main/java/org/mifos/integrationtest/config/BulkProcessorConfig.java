23f1032547a52b487ee5263737b1e05cb2f27c7c
Ankita Ranjan <92036361+ankita10r@users.noreply.github.com>
2023-05-25 12:44:18 +0530

    Adding integration test for callback (#8)

--- src/main/java/org/mifos/integrationtest/config/BulkProcessorConfig.java
+++ src/main/java/org/mifos/integrationtest/config/BulkProcessorConfig.java
@@ -16,9 +16,29 @@ public class BulkProcessorConfig {
 
     public String bulkTransactionUrl;
 
+    public String callbackUrl;
+
+   public int retryCount;
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
     @PostConstruct
     private void setup() {
         bulkTransactionUrl = bulkProcessorContactPoint + bulkTransactionEndpoint;
     }
 
+
+    public String getCallbackUrl() {
+        return callbackUrl;
+    }
+
+    public void setCallbackUrl(String callbackUrl) {
+        this.callbackUrl = callbackUrl;
+    }
 }
