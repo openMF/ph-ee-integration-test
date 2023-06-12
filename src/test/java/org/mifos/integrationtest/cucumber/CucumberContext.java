04a460fd278e5744f51f29352d6411e0d728645c
DanishJamal <danishjamal.104@gmail.com>
2023-06-12 12:45:43 +0530

    Fixes PHEE-311 Connector-common upgraded to 1.5.0 maintaining the stability

--- src/test/java/org/mifos/integrationtest/cucumber/CucumberContext.java
+++ src/test/java/org/mifos/integrationtest/cucumber/CucumberContext.java
@@ -1,13 +1,18 @@
 package org.mifos.integrationtest.cucumber;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mifos.integrationtest.IntegrationTestApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @CucumberContextConfiguration
 @CucumberOptions(publish = true)
-@SpringBootTest
+@RunWith(Cucumber.class)
+@SpringBootTest(classes = IntegrationTestApplication.class)
 public class CucumberContext {
 
     @Test
