4ade5a67b85c8c47de9e90e6be9696b3c21b20d9
Arka Das <arka.das@fynarfin.io>
2023-07-24 15:48:59 +0530

    GOV-301: Batch Authorization integration test
    GOV-301: update class name
    
    GOV-301: add @gov to batch auth integration test
    
    GOV-301: refactored test
    
    GOV-301: updated integration test with wiremock
    
    GOV-301: TODO add ingress for mock payment schema
    
    GOV-301: add ingress for mock payment schema
    
    GOV-301: remove duplicate wire mock started
    
    GOV-301: add @gov annotation to integration test
    
    GOV-301: working integration test
    
    GOV-301: test in sit
    
    add indentation

---
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/BatchAuthorizationStepDef.java
@@ -0,0 +1,68 @@
+package org.mifos.integrationtest.cucumber.stepdef;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.mifos.integrationtest.common.AuthorizationRequest;
+import org.mifos.integrationtest.common.BatchSummaryResponse;
+import org.mifos.integrationtest.common.Utils;
+import org.mifos.integrationtest.config.MockPaymentSchemaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+public class BatchAuthorizationStepDef extends BaseStepDef {
+
+    @Autowired
+    MockPaymentSchemaConfig mockPaymentSchemaConfig;
+
+    @Value("${callback_url}")
+    private String callBackUrl;
+
+    private static AuthorizationRequest authorizationRequest;
+
+    @Then("I should get batch status as {string}")
+    public void iShouldGetBatchStatusAs(String status) {
+        String response = BaseStepDef.response;
+        BatchSummaryResponse batchSummaryResponse;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            batchSummaryResponse = objectMapper.readValue(response, BatchSummaryResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        assertThat(batchSummaryResponse.getStatus()).isEqualTo(status);
+    }
+
+    @Then("I should be able to verify that the {string} method to {string} endpoint received a request with authorization status")
+    public void iShouldBeAbleToVerifyThatTheMethodToEndpointReceivedARequestWithAuthorizationStatus(String httpMethod, String endpoint) {
+        verify(postRequestedFor(urlEqualTo(endpoint))
+                .withRequestBody(matchingJsonPath("$.status", equalTo("Y"))));
+    }
+
+    @When("I call the Authorization API with batchId as {string} and expected status of {int} and stub {string}")
+    public void iCallTheAuthorizationAPIWithBatchIdAsAndExpectedStatusOfAndStub(String batchId, int expectedStatus, String stub) {
+        RequestSpecification requestSpec = Utils.getDefaultSpec();
+        BaseStepDef.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
+                .header("X-CallbackURL", callBackUrl + stub)
+                .header("X-Client-Correlation-ID", "998877")
+                .queryParam("command", "authorize")
+                .baseUri(mockPaymentSchemaConfig.mockPaymentSchemaContactPoint).body(authorizationRequest)
+                .expect().spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
+                .post(mockPaymentSchemaConfig.mockBatchAuthorizationEndpoint + batchId).andReturn().asString();
+
+        logger.info("Authorization Response: {}", BaseStepDef.response);
+    }
+
+    @When("I create an AuthorizationRequest for Batch Authorization with batch ID as {string}, payerIdentifier as {string}, currency as {string} and amount as {string}")
+    public void iCreateAnAuthorizationRequestForBatchAuthorizationWithBatchIDAsPayerIdentifierAsCurrencyAsAndAmountAs
+            (String batchId, String payerIdentifier, String currency, String amount) {
+        authorizationRequest = new AuthorizationRequest(batchId, payerIdentifier, currency, amount);
+    }
+}
