61b2e528483509dad486bccfd07339aa2d99df40
Dhruv Sonagara <dhruvsonagara@Dhruvs-MacBook-Pro.local>
2023-09-01 10:40:21 +0530

    Reverted unnescessary changes

--- src/test/java/org/mifos/integrationtest/cucumber/stepdef/BatchApiStepDef.java
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/BatchApiStepDef.java
@@ -70,7 +70,7 @@ public class BatchApiStepDef extends BaseStepDef {
         assertThat(BaseStepDef.batchId).isNotEmpty();
     }
 
-    @Given("I ha    ve the demo csv file {string}")
+    @Given("I have the demo csv file {string}")
     public void setFilename(String filename) {
         BaseStepDef.filename = filename;
         File f = new File(Utils.getAbsoluteFilePathToResource(BaseStepDef.filename));
