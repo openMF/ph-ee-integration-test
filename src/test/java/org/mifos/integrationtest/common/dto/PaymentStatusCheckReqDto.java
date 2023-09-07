2abef07a8678621f7667a174f3042d77b1ee2025
Ankita Ranjan <92036361+ankita10r@users.noreply.github.com>
2023-09-08 00:53:35 +0530

    Integration and component test for GOV-158 (#127)

---
+++ src/test/java/org/mifos/integrationtest/common/dto/PaymentStatusCheckReqDto.java
@@ -0,0 +1,40 @@
+package org.mifos.integrationtest.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class PaymentStatusCheckReqDto {
+
+    @Override
+    public String toString() {
+        return "PaymentStatusCheckReqDto{" +
+                "requestIds:" + requestIds +
+                ", payeePartyIds:" + payeePartyIds +
+                '}';
+    }
+
+    List<String> requestIds;
+    List<String>  payeePartyIds;
+
+    public PaymentStatusCheckReqDto() {
+
+    }
+
+    public List<String> getRequestIds() {
+        return requestIds;
+    }
+
+    public void setRequestIds(List<String> requestIds) {
+        this.requestIds = requestIds;
+    }
+
+    public List<String>  getPayeePartyIds() {
+        return payeePartyIds;
+    }
+
+    public void setPayeePartyIds(List<String>  payeePartyIds) {
+        this.payeePartyIds = payeePartyIds;
+    }
+}
+
