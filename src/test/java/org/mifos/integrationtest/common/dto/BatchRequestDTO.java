3be9daf719a8666421954b00e87cfdbe26304061
Danish Jamal <31315800+danishjamal104@users.noreply.github.com>
2023-09-07 23:06:40 +0530

    PHEE-415: Added test case for the batch rest json based api (#128)
    * Added test case for the batch rest json based api
    
    * Enhanced the test casse
    
    * Fixed one of the step in BD-005
    
    * Updated the test case

---
+++ src/test/java/org/mifos/integrationtest/common/dto/BatchRequestDTO.java
@@ -0,0 +1,19 @@
+package org.mifos.integrationtest.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BatchRequestDTO {
+
+    List<Party> creditParty, debitParty;
+
+    String subType, amount, currency, descriptionText;
+
+}
