26581d3ba207fbc708aa8451eafcac723db3ec2a
Apurb Rajdhan <103931815+apurbraj@users.noreply.github.com>
2024-02-14 15:08:30 +0530

    checkstyle

--- src/main/java/org/mifos/integrationtest/config/FspConfig.java
+++ src/main/java/org/mifos/integrationtest/config/FspConfig.java
@@ -2,7 +2,6 @@ package org.mifos.integrationtest.config;
 
 import java.util.HashMap;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
