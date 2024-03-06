a8e5608479e148474b11029e0d5828453a493ab5
dhruvsonagara <dhruvsonagara@gmail.com>
2024-03-06 16:04:27 +0530

    updated the test results

--- src/test/java/org/mifos/integrationtest/TestRunner.java
+++ src/test/java/org/mifos/integrationtest/TestRunner.java
@@ -14,10 +14,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Courgette.class)
 @CourgetteOptions(threads = 3, runLevel = CourgetteRunLevel.SCENARIO, rerunFailedScenarios = false,
-        // rerunAttempts = ,
         testOutput = CourgetteTestOutput.CONSOLE,
 
-        reportTitle = "Courgette-JVM Example", reportTargetDir = "build", environmentInfo = "browser=chrome; git_branch=master", cucumberOptions = @CucumberOptions(features = "src/test/java/resources", glue = "org.mifos.integrationtest.cucumber", tags = "@gov", publish = true, plugin = {
+        reportTitle = "Courgette-JVM Example", reportTargetDir = "build", cucumberOptions = @CucumberOptions(features = "src/test/java/resources", glue = "org.mifos.integrationtest.cucumber", tags = "@gov", publish = true, plugin = {
                 "html:cucumber-report", "json:cucumber.json", "pretty", "html:build/cucumber-report.html",
                 "json:build/cucumber-report.json", "junit:build/cucumber.xml" }))
 public class TestRunner {}
