a23c5aac3c31eee0a1cc5c70a9e91bc0586d0c8e
dhruvsonagara <dhruvsonagara@gmail.com>
2024-06-21 12:24:15 +0530

    Added the get transfer date range,amount and currency related TCs

--- src/test/java/org/mifos/integrationtest/cucumber/stepdef/ChannelClientIdDef.java
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/ChannelClientIdDef.java
@@ -2,6 +2,9 @@ package org.mifos.integrationtest.cucumber.stepdef;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
@@ -11,6 +14,9 @@ import org.mifos.integrationtest.common.Utils;
 import org.mifos.integrationtest.config.ChannelConnectorConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ChannelClientIdDef extends BaseStepDef {
 
@@ -82,5 +88,109 @@ public class ChannelClientIdDef extends BaseStepDef {
                 .asString();
 
         logger.info("Inbound transfer with size and page Response: {}", scenarioScopeState.response);
+    }
+
+    @When("I call the transfer API with specific date range expecting expected status of {int}")
+    public void iCallTheTransferAPIWithSpecificDateRangeExpectingExpectedStatusOf(int expectedStatus) {
+        RequestSpecification requestSpec = Utils.getDefaultSpec(scenarioScopeState.tenant);
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(5);
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+
+        String startfrom = startDate.format(formatter);
+        String startto = endDate.format(formatter);
+
+        String endpoint = String.format("%s?startfrom=%s&startto=%s",
+                operationsAppConfig.transfersEndpoint, startfrom, startto);
+        String fullUrl = operationsAppConfig.operationAppContactPoint + endpoint;
+
+        logger.info("Calling endpoint: {}", fullUrl);
+
+        scenarioScopeState.response = RestAssured.given(requestSpec)
+                .baseUri(operationsAppConfig.operationAppContactPoint)
+                .expect()
+                .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build())
+                .when()
+                .get(endpoint)
+                .andReturn()
+                .asString();
+
+        logger.info("Inbound transfer with date range Response: {}", scenarioScopeState.response);
+    }
+
+    @And("I should have page and size in response")
+    public void iShouldHavePageAndSizeInResponse() throws JsonProcessingException {
+        String response = scenarioScopeState.response;
+
+        // Parse the JSON response
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response);
+
+        // Check that 'size' and 'number' are present
+        assertThat(jsonNode.has("size")).isTrue();
+        assertThat(jsonNode.has("number")).isTrue();
+
+        // Check the exact values
+        int expectedSize = 4;
+        int expectedNumber = 2;
+        assertThat(jsonNode.get("size").asInt()).isEqualTo(expectedSize);
+        assertThat(jsonNode.get("number").asInt()).isEqualTo(expectedNumber);
+    }
+
+    @When("I call the transfer API with currency {string} and amount {int} expecting expected status of {int}")
+    public void iCallTheTransferAPIWithCurrencyAndAmountExpectingExpectedStatusOf(String currency, int amount, int expectedStatus) {
+        RequestSpecification requestSpec = Utils.getDefaultSpec(scenarioScopeState.tenant);
+        if (authEnabled) {
+            requestSpec.header("Authorization", "Bearer " + scenarioScopeState.accessToken);
+        }
+
+        String endpoint = String.format("%s?currency=%s&amount=%d", operationsAppConfig.transfersEndpoint, currency, amount);
+        logger.info("Calling get transfer endpoint: {}", endpoint);
+
+        scenarioScopeState.response = RestAssured.given(requestSpec)
+                .baseUri(operationsAppConfig.operationAppContactPoint)
+                .expect()
+                .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build())
+                .when()
+                .get(endpoint)
+                .andReturn()
+                .asString();
+
+        logger.info("Inbound transfer with currency and amount Response: {}", scenarioScopeState.response);
+    }
+
+    @And("I should have currency and amount in response")
+    public void iShouldHaveCurrencyAndAmountInResponse() throws JsonProcessingException {
+        String response = scenarioScopeState.response;
+
+        // Parse the JSON response
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response);
+
+        assertThat(jsonNode.has("content")).isTrue();
+        assertThat(jsonNode.get("content").isArray()).isTrue();
+        assertThat(jsonNode.get("content").size()).isGreaterThan(0);
+
+        // Extract the first object in the 'content' array
+        JsonNode firstContentItem = jsonNode.get("content").get(0);
+
+        // Check that 'currency' and 'amount' are present in the first content item
+        assertThat(firstContentItem.has("currency")).isTrue();
+        assertThat(firstContentItem.has("amount")).isTrue();
+
+        // Retrieve and validate the currency
+        JsonNode currencyNode = firstContentItem.get("currency");
+        assertThat(currencyNode).isNotNull();
+        String currency = currencyNode.asText();
+        String expectedCurrency = "USD";
+        assertThat(currency).isEqualTo(expectedCurrency);
+
+        // Retrieve and validate the amount
+        JsonNode amountNode = firstContentItem.get("amount");
+        assertThat(amountNode).isNotNull();
+        String amountAsString = amountNode.asText();
+        int amount = Integer.parseInt(amountAsString);
+        int expectedAmount = 1;
+        assertThat(amount).isEqualTo(expectedAmount);
     }
 }
--- src/test/java/resources/getTransfer.feature
+++ src/test/java/resources/getTransfer.feature
@@ -3,7 +3,7 @@
 Feature: Get Transfers API test
 
 
-        Scenario: GT-001 Get Transfers API With Auth
+        Scenario: GT-001 Get Transfers API With default params
                 Given I have tenant as "paymentBB2"
                 When I call the operations-app auth endpoint with username: "mifos" and password: "password"
                 Then I should get a valid token
@@ -18,3 +18,19 @@ Feature: Get Transfers API test
                 Then I should get a valid token
                 When I call the transfer API with size 4 and page 2 expecting expected status of 200
                 Then I should get non empty response
+                And I should have page and size in response
+
+        Scenario: GT-003 Get Transfers API Within specific date range
+                Given I have tenant as "paymentBB2"
+#                When I call the operations-app auth endpoint with username: "mifos" and password: "password"
+                Then I should get a valid token
+                When I call the transfer API with specific date range expecting expected status of 200
+                Then I should get non empty response
+
+        Scenario: GT-004 Get Transfers API With amount and currency
+                Given I have tenant as "paymentBB2"
+#                When I call the operations-app auth endpoint with username: "mifos" and password: "password"
+                Then I should get a valid token
+                When I call the transfer API with currency "USD" and amount 1 expecting expected status of 200
+                Then I should get non empty response
+                And I should have currency and amount in response
